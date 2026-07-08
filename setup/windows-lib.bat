65175e55ace43177b836a6317bac80e8742f9784
MTGC <satriyoadi130@gmail.com>
2026-07-08 13:30:56 +0700

    Updated OpenFL version [skip ci]

--- setup/windows-lib.bat
+++ setup/windows-lib.bat
@@ -9,54 +9,51 @@ echo This may take a few moments.
 echo.
 
 :: Core
-echo [1/16] hxcpp
+echo [1/15] hxcpp
 haxelib git hxcpp https://github.com/Psych-Plus-Team/hxcpp --quiet
 
-echo [2/16] lime
+echo [2/15] lime
 haxelib git lime https://github.com/Psych-Plus-Team/lime.git --quiet    
 
-echo [3/16] openfl
+echo [3/15] openfl
 haxelib install openfl 9.5.2 --quiet
 
-echo [4/16] datetime
-haxelib install datetime 3.1.4 --quiet
-
 :: HaxeFlixel
-echo [5/16] flixel
+echo [4/15] flixel
 haxelib git flixel https://github.com/Psych-Plus-Team/flixel --quiet
 
-echo [6/16] flixel-addons
+echo [5/15] flixel-addons
 haxelib install flixel-addons 3.3.2 --quiet
 
-echo [7/16] flixel-tools
+echo [6/15] flixel-tools
 haxelib install flixel-tools 1.5.1 --quiet
 
 :: Engine
-echo [8/16] hscript-iris
+echo [7/15] hscript-iris
 haxelib git hscript-iris https://github.com/Psych-Plus-Team/hscript-iris.git --quiet
 
-echo [9/16] moonchart
+echo [8/15] moonchart
 haxelib install moonchart 0.5.1 --quiet
 
-echo [10/16] tjson
+echo [9/15] tjson
 haxelib install tjson 1.4.0 --quiet
 
-echo [11/16] flxanimate
+echo [10/15] flxanimate
 haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e --quiet
 
-echo [12/16] linc_luajit
+echo [11/15] linc_luajit
 haxelib git linc_luajit https://github.com/Psych-Plus-Team/linc_luajit.git --quiet
 
-echo [13/16] hxdiscord_rpc
+echo [12/15] hxdiscord_rpc
 haxelib install hxdiscord_rpc --quiet --skip-dependencies
 
-echo [14/16] hxvlc
+echo [13/15] hxvlc
 haxelib install hxvlc 2.2.6 --quiet --skip-dependencies
 
-echo [15/16] funkin.vis
+echo [14/15] funkin.vis
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90 --quiet
 
-echo [16/16] grig.audio
+echo [15/15] grig.audio
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666 --quiet
 
 echo.
